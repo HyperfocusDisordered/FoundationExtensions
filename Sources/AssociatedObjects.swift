7d9864e3d59c342825b9a09fc23b8fc240b8fa71
DenisOvchat <fitil457@gmail.com>
2023-11-16 07:42:13 +0530

    reorganizing...

---
+++ Sources/AssociatedObjects.swift
@@ -0,0 +1,30 @@
+//
+//  AssociatedObjects.swift
+//  StoriesLMS
+//
+//  Created by  Denis Ovchar new on 21.05.2021.
+//
+
+import Foundation
+
+
+public func associatedObject<ValueType: AnyObject>(
+    base: AnyObject,
+    key: UnsafePointer<UInt8>,
+    initialiser: () -> ValueType)
+    -> ValueType {
+        if let associated = objc_getAssociatedObject(base, key)
+            as? ValueType { return associated }
+        let associated = initialiser()
+        objc_setAssociatedObject(base, key, associated,
+                                 .OBJC_ASSOCIATION_RETAIN)
+        return associated
+}
+
+public func associateObject<ValueType: AnyObject>(
+    base: AnyObject,
+    key: UnsafePointer<UInt8>,
+    value: ValueType) {
+    objc_setAssociatedObject(base, key, value,
+                             .OBJC_ASSOCIATION_RETAIN)
+}
